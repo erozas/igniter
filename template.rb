7ce050d6143ae7f6a8136d9f9327444c14d55656
erozas <exequiel98@gmail.com>
2018-11-28 21:07:01 -0300

    WIP

--- template.rb
+++ template.rb
@@ -6,27 +6,23 @@ require "shellwords"
 # copy_file and template resolve against our source files. If this file was
 # invoked remotely via HTTP, that means the files are not present locally.
 # In that case, use `git clone` to download them to a local temporary dir.
-# def add_template_repository_to_source_path
-#   if __FILE__ =~ %r{\Ahttps?://}
-#     require "tmpdir"
-#     source_paths.unshift(tempdir = Dir.mktmpdir("igniter"))
-#     at_exit { FileUtils.remove_entry(tempdir) }
-#     git clone: [
-#       "--quiet",
-#       "https://github.com/erozas/igniter.git",
-#       tempdir
-#     ].map(&:shellescape).join(" ")
+def add_template_repository_to_source_path
+  if __FILE__ =~ %r{\Ahttps?://}
+    require "tmpdir"
+    source_paths.unshift(tempdir = Dir.mktmpdir("igniter"))
+    at_exit { FileUtils.remove_entry(tempdir) }
+    git clone: [
+      "--quiet",
+      "https://github.com/erozas/igniter.git",
+      tempdir
+    ].map(&:shellescape).join(" ")
 
-#     if (branch = __FILE__[%r{igniter/(.+)/template.rb}, 1])
-#       Dir.chdir(tempdir) { git checkout: branch }
-#     end
-#   else
-#     source_paths.unshift(File.dirname(__FILE__))
-#   end
-# end
-
-def source_paths
-  [File.expand_path(File.dirname(__FILE__))]
+    if (branch = __FILE__[%r{igniter/(.+)/template.rb}, 1])
+      Dir.chdir(tempdir) { git checkout: branch }
+    end
+  else
+    source_paths.unshift(File.dirname(__FILE__))
+  end
 end
 
 def add_gems
@@ -225,8 +221,7 @@ def add_sitemap
 end
 
 # Main setup
-# add_template_repository_to_source_path
-source_paths
+add_template_repository_to_source_path
 
 add_gems
 
